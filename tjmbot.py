2f190753df24df440b4dcd4603d69e83e1857981
TJM <tommy.mairo@gmail.com>
2016-01-05 22:39:40 +0800

    Trans to python3

--- tjmbot.py
+++ tjmbot.py
@@ -16,24 +16,24 @@ def main(network, nick, chan, port, password):
   socket.connect((network,port))
   irc = ssl.wrap_socket(socket)
   def speak(message):
-    irc.send('PRIVMSG #%s :%s\r\n' % (chan,message))
+    irc.send(('PRIVMSG #%s :%s\r\n' % (chan,message)).encode('utf-8'))
   irc.send('NICK %s\r\n' % nick)
   time.sleep(1)
   print (irc.recv(4096))
-  irc.send('USER %s %s %s :My bot\r\n' % (nick,nick,nick))
+  irc.send(('USER %s %s %s :My bot\r\n' % (nick,nick,nick)).encode('utf-8'))
   print (irc.recv(4096))
   time.sleep(1)
-  irc.send('PRIVMSG NickServ :IDENTIFY %s\r\n' % password)
+  irc.send(('PRIVMSG NickServ :IDENTIFY %s\r\n' % password).encode('utf-8'))
   print (irc.recv(4096))
   time.sleep(3)
-  irc.send('JOIN #%s\r\n' % chan)
+  irc.send(('JOIN #%s\r\n' % chan).encode('utf-8'))
   print (irc.recv(4096))
   time.sleep(1)
   while True:
     data = (irc.recv(4096))
     print (data)
     if data.find('PING') != -1:
-      irc.send('PONG '+data.split()[1]+'\r\n')
+      irc.send(('PONG '+data.split()[1]+'\r\n').encode('utf-8'))
     if data.find('摸摸') != -1:
       speak('不哭不哭 站起来撸')
     if data.find('`嚎吧') != -1:
@@ -41,7 +41,7 @@ def main(network, nick, chan, port, password):
     if (data.find('逃~') != -1) or (data.find('( 逃') != -1) :
       speak('逃什么逃！你丫就是一个没对象的野指针，哪会有人追你！\r\n')
     if data.find('`Shut up tjmbot!\r\n') != -1:
-      irc.send('QUIT :吾去矣 \r\n')
+      irc.send('QUIT :吾去矣 \r\n'.encode('utf-8'))
       time.sleep(1)
       exit()
 	  
