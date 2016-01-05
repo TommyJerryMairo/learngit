4dd9dc21c2d5ab08e15f25644345348375c01103
TJM <tommy.mairo@google.com>
2016-01-05 06:07:43 -0500

    Build TJMBot.

--- tjmbot.py
+++ tjmbot.py
@@ -15,21 +15,24 @@ def main(network, nick, chan, port, password):
   socket.connect((network,port))
   irc = ssl.wrap_socket(socket)
   irc.send('NICK %s\r\n' % nick)
+  time.sleep(1)
   print irc.recv(4096)
   irc.send('USER %s %s %s :My bot\r\n' % (nick,nick,nick))
   print irc.recv(4096)
+  time.sleep(1)
   irc.send('PRIVMSG NickServ :IDENTIFY %s\r\n' % password)
   print irc.recv(4096)
   time.sleep(3)
   irc.send('JOIN #%s\r\n' % chan)
   print irc.recv(4096)
+  time.sleep(1)
   while True:
     data = irc.recv(4096)
     print data
     if data.find('PING') != -1:
       irc.send('PONG '+data.split()[1]+'\r\n')
     if data.find('摸摸') != -1:
-      data.send('不哭不哭 站起来撸\r\n')
+      irc.send('不哭不哭 站起来撸\r\n')
     if data.find('`Shut up tjmbot!\r\n') != -1:
       irc.send('QUIT\r\n')
       exit()
