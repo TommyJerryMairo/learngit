28eba1c80fb62c13d47eb19a96b9d9739d5c8996
TJM <tommy.mairo@google.com>
2016-01-14 10:02:58 -0500

    better

--- mzx/homework1.py
+++ mzx/homework1.py
@@ -6,9 +6,10 @@ import string
 def main():
     mengzhaoxin=list(string.ascii_letters)
     mengzhaoxin.extend(range(10))
+    mengzhaoxin=list(map(lambda x: str(x),mengzhaoxin))
     for i in range(200):
         random.shuffle(mengzhaoxin)
-		print ''.join(mengzhaoxin[:20])
+	print ''.join(mengzhaoxin[:20])
 
 if __name__=='__main__':
     main()
