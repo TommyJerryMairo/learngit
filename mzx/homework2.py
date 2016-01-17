b4f4c4935aea3e5604684c0bc7a453c5b24268b2
TJM <tommy.mairo@gmail.com>
2016-01-17 13:27:55 +0800

    Upgrade

--- mzx/homework2.py
+++ mzx/homework2.py
@@ -8,15 +8,15 @@ import sys
 def main(u,p,d):
     mengzhaoxin=list(string.ascii_letters)
     mengzhaoxin.extend(range(10))
-	conn=mysql.connector.connect(user=u,password=p,database=d)
-	cursor=conn.cursor()
-	cursor.execute('create table user (id int primary key, verification varchar(20))')
+    conn=mysql.connector.connect(user=u,password=p,database=d)
+    cursor=conn.cursor()
+    cursor.execute('create table user (id int primary key, verification varchar(20))')
     for i in range(200):
         random.shuffle(mengzhaoxin)
 	cursor.execute('insert into user (id, name) values (%s, %s)', [i, ''.join(mengzhaoxin[:20])])
 	cursor.rowcount
-	conn.commit()
-	cursor.close()
+    conn.commit()
+    cursor.close()
 		
 if __name__=='__main__':
     main(sys.argv[1],sys.argv[2],sys.argv[3])
